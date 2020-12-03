9b73f5f60de35be8114441deb44158233df7a9e6
RypoFalem <rypofalem@gmail.com>
2020-12-03 00:07:27 -0600

    variable names, reimplement calcCollisions shorter and faster

--- src/main/scala/io/github/rypofalem/aoc2020/Days/Day03.scala
+++ src/main/scala/io/github/rypofalem/aoc2020/Days/Day03.scala
@@ -4,22 +4,14 @@ import io.github.rypofalem.aoc2020.Problem
 
 object Day03 extends Problem{
   override def uid: Int = 3
-  lazy val terrainPattern: Vector[String] = lines.toVector
+  lazy val terrain: Vector[String] = lines.toVector
   val tree: Char = '#'
-  val patternLength: Int = terrainPattern(0).length
+  lazy val terrainWidth: Int = terrain(0).length
 
-  def isTree(down:Int, right:Int): Boolean = terrainPattern(down).charAt(right % patternLength) == tree
+  def isTree(down:Int, right:Int): Boolean = terrain(down).charAt(right % terrainWidth) == tree
 
-  def calcCollisions(d :Int, r:Int): Int = {
-    val treeCollisions = for {
-      i <- 1 until terrainPattern.length
-      right = i * r
-      down = i * d
-      if down < terrainPattern.length
-      if isTree(down, right)
-    } yield (down, right)
-    treeCollisions.length
-  }
+  def calcCollisions(downSpeed: Int, rightSpeed: Int): Int =
+    (1 until terrain.length / downSpeed) count (move => isTree(move * downSpeed, move * rightSpeed))
 
   override def solution1: String = {
     val collisions = calcCollisions(1,3)
