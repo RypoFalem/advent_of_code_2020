8d53e14f8d6de7f5b8c887da1f67f52337683f01
RypoFalem <rypofalem@gmail.com>
2020-12-06 01:20:08 -0600

    move the error handling into main

--- src/main/scala/io/github/rypofalem/aoc2020/Problem.scala
+++ src/main/scala/io/github/rypofalem/aoc2020/Problem.scala
@@ -6,20 +6,19 @@ trait Problem {
   def uid: Int
   def solution1: String
   def solution2: String
+  override def toString: String = s"Problem $uid:\n\t$solution1\n\t$solution2"
+  lazy val lines: List[String] = Util.loadResourceAsList(f"Day$uid%02d.txt")
 
-  override def toString: String = {
+  def main(args: Array[String]): Unit ={
     def trySolution(solution: => String): String = Try(solution) match {
       case Success(value) => value
       case Failure(exception) =>
         exception.printStackTrace()
         exception.toString
     }
+
     val s1 = trySolution(solution1)
     val s2 = trySolution(solution2)
-    s"Problem $uid:\n\t$s1\n\t$s2"
+    println(s"Problem $uid:\n\t$s1\n\t$s2")
   }
-
-  def lines: List[String] = Util.loadResourceAsList(f"Day$uid%02d.txt")
-
-  def main(args: Array[String]): Unit = println(toString)
 }
