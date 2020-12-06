259f8a9a045fbdfe45e5aba2290662d105c83b80
RypoFalem <rypofalem@gmail.com>
2020-12-05 23:31:18 -0600

    Day06

---
+++ src/main/scala/io/github/rypofalem/aoc2020/Days/Day06.scala
@@ -0,0 +1,25 @@
+package io.github.rypofalem.aoc2020.Days
+
+import io.github.rypofalem.aoc2020.Problem
+
+object Day06 extends Problem{
+  override def uid: Int = 6
+  val groups: Array[String] = lines.mkString("\n").split("\n\n")
+
+  lazy val countYes: Array[Int] = {
+    def uniqueLetters(str: String): Int = str.toSet.count(_.isLetter)
+    groups map uniqueLetters
+  }
+
+  lazy val countAllYes: Array[Int] = {
+    def uniqueLetters(str: String): Int =
+      str.split("\n").map(_.toSet).reduce[Set[Char]]((a,b) => a intersect b).count(_.isLetter)
+    groups map uniqueLetters
+  }
+
+  override def solution1: String = s"The sum of all unique yes's in all groups is ${countYes.sum}"
+
+  override def solution2: String = s"The sum of all unique unanimous yes's in all groups is ${countAllYes.sum}"
+
+  def main(args: Array[String]): Unit = println(toString)
+}
